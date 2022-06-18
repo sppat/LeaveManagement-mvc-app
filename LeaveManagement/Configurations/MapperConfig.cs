de2a134dec21f4fbce586ae00bf04cbc9e1014e7
Spyros Paterakis <spyridon.paterakis@outlook.com.gr>
2022-06-18 11:18:19 +0300

    introduce viewmodel and made ui changes

---
+++ LeaveManagement/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LeaveManagement.Data;
+using LeaveManagement.Models;
+
+namespace LeaveManagement.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
--- LeaveManagement/Program.cs
+++ LeaveManagement/Program.cs
@@ -1,5 +1,5 @@
+using LeaveManagement.Configurations;
 using LeaveManagement.Data;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +12,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
